699f89e5874aff069cd52d60efc372238fc238cf
NITIN ANARWAL <NITINANARWAL@GMAIL.COM>
2021-03-26 14:59:29 +0530

    basic page setup

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 "react-dom": "^16.13.1",
 "react-scripts": "3.4.3",
 ```
+
+### FormSpree For Newsletter
+
+https://formspree.io/
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            exercitationem in aliquid animi corporis, tempore neque tempora
+            earum, quae cum est, veritatis assumenda sint magnam autem officiis
+            accusamus reiciendis illo.
+          </p>
+          <form
+            type='submit'
+            className='contact-form'
+            action='https://formspree.io/f/xgerwpwa'
+            method='POST'
+          >
+            <input
+              type='email'
+              placeholder='Enter Email'
+              className='form-input'
+              name='_replyto'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +97,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
