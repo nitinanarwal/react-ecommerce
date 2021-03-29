0daaf87bf5c6a808e34d160dcef9b2ea9d5fb026
NITIN ANARWAL <NITINANARWAL@GMAIL.COM>
2021-03-29 06:03:41 +0530

    basic single product page setup

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -13,12 +13,15 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_OPEN) {
     return { ...state, isSidebarOpen: true };
   }
+
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
+
   if (action.type === GET_PRODUCTS_BEGIN) {
     return { ...state, products_loading: true };
   }
+
   if (action.type === GET_PRODUCTS_SUCCESS) {
     const featured_products = action.payload.filter(
       (product) => product.featured === true
@@ -30,9 +33,36 @@ const products_reducer = (state, action) => {
       featured_products,
     };
   }
+
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_error: true, products_loading: false };
   }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: false,
+      single_product: action.payload,
+    };
+  }
+
   throw new Error(`No Matching ${action.type} - action type`);
 };
 export default products_reducer;
